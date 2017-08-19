28f23edf39e73dd884e905bfb37308cedd8c7bb8
ZzzD <zddlfree@gmail.com>
2017-08-19 21:24:45 +0800

    auto select lambda

--- Machine Learning/ex5/ex5.m
+++ Machine Learning/ex5/ex5.m
@@ -34,96 +34,96 @@ load ('ex5data1.mat');
 % m = Number of examples
 m = size(X, 1);
 
-% Plot training data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
+% % Plot training data
+% plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% xlabel('Change in water level (x)');
+% ylabel('Water flowing out of the dam (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
-%% =========== Part 2: Regularized Linear Regression Cost =============
-%  You should now implement the cost function for regularized linear 
-%  regression. 
-%
+% %% =========== Part 2: Regularized Linear Regression Cost =============
+% %  You should now implement the cost function for regularized linear 
+% %  regression. 
+% %
 
-theta = [1 ; 1];
-J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+% theta = [1 ; 1];
+% J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Cost at theta = [1 ; 1]: %f '...
-         '\n(this value should be about 303.993192)\n'], J);
+% fprintf(['Cost at theta = [1 ; 1]: %f '...
+%          '\n(this value should be about 303.993192)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
-%% =========== Part 3: Regularized Linear Regression Gradient =============
-%  You should now implement the gradient for regularized linear 
-%  regression.
-%
+% %% =========== Part 3: Regularized Linear Regression Gradient =============
+% %  You should now implement the gradient for regularized linear 
+% %  regression.
+% %
 
-theta = [1 ; 1];
-[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+% theta = [1 ; 1];
+% [J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
-         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
-         grad(1), grad(2));
+% fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
+%          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
+%          grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-
-%% =========== Part 4: Train Linear Regression =============
-%  Once you have implemented the cost and gradient correctly, the
-%  trainLinearReg function will use your cost function to train 
-%  regularized linear regression.
-% 
-%  Write Up Note: The data is non-linear, so this will not give a great 
-%                 fit.
-%
-
-%  Train linear regression with lambda = 0
-lambda = 0;
-[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
-
-%  Plot fit over the data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-hold on;
-plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
-hold off;
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
-%% =========== Part 5: Learning Curve for Linear Regression =============
-%  Next, you should implement the learningCurve function. 
-%
-%  Write Up Note: Since the model is underfitting the data, we expect to
-%                 see a graph with "high bias" -- Figure 3 in ex5.pdf 
-%
+% %% =========== Part 4: Train Linear Regression =============
+% %  Once you have implemented the cost and gradient correctly, the
+% %  trainLinearReg function will use your cost function to train 
+% %  regularized linear regression.
+% % 
+% %  Write Up Note: The data is non-linear, so this will not give a great 
+% %                 fit.
+% %
 
-lambda = 0;
-[error_train, error_val] = ...
-    learningCurve([ones(m, 1) X], y, ...
-                  [ones(size(Xval, 1), 1) Xval], yval, ...
-                  lambda);
+% %  Train linear regression with lambda = 0
+% lambda = 0;
+% [theta] = trainLinearReg([ones(m, 1) X], y, lambda);
 
-plot(1:m, error_train, 1:m, error_val);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 150])
+% %  Plot fit over the data
+% plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% xlabel('Change in water level (x)');
+% ylabel('Water flowing out of the dam (y)');
+% hold on;
+% plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
+% hold off;
 
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+
+% %% =========== Part 5: Learning Curve for Linear Regression =============
+% %  Next, you should implement the learningCurve function. 
+% %
+% %  Write Up Note: Since the model is underfitting the data, we expect to
+% %                 see a graph with "high bias" -- Figure 3 in ex5.pdf 
+% %
+
+% lambda = 0;
+% [error_train, error_val] = ...
+%     learningCurve([ones(m, 1) X], y, ...
+%                   [ones(size(Xval, 1), 1) Xval], yval, ...
+%                   lambda);
+
+% plot(1:m, error_train, 1:m, error_val);
+% title('Learning curve for linear regression')
+% legend('Train', 'Cross Validation')
+% xlabel('Number of training examples')
+% ylabel('Error')
+% axis([0 13 0 150])
+
+% fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+% for i = 1:m
+%     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+% end
+
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %  One solution to this is to use polynomial regression. You should now
@@ -149,11 +149,11 @@ X_poly_val = bsxfun(@minus, X_poly_val, mu);
 X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
 X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
 
-fprintf('Normalized Training Example 1:\n');
-fprintf('  %f  \n', X_poly(1, :));
+% fprintf('Normalized Training Example 1:\n');
+% fprintf('  %f  \n', X_poly(1, :));
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+% fprintf('\nProgram paused. Press enter to continue.\n');
+% pause;
 
 
 
@@ -164,7 +164,7 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 3;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
